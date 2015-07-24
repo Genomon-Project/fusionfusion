29dcac1eec209f240b307c2cefce1c43f941aaa1
friend1ws <friend1ws@gmail.com>
2015-07-24 17:01:44 +0900

    made devel branch

---
+++ lib/fusionfusion/run.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+def main(args):
+
+    # should add validity check for arguments
+    targetMutationFile = args.targetMutationFile
+    targetBamPath = args.targetBamPath
+    controlBamPathList = args.controlBamPathList
+    outputPath = args.outputPath
+
+
