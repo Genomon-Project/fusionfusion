7e33f755f10d6d570c723c1b5f4238e71504a866
friend1ws <friend1ws@gmail.com>
2015-07-25 16:07:28 +0900

    create __init__.py

--- lib/fusionfusion/run.py
+++ lib/fusionfusion/run.py
@@ -2,10 +2,8 @@
 
 def main(args):
 
-    # should add validity check for arguments
-    targetMutationFile = args.targetMutationFile
-    targetBamPath = args.targetBamPath
-    controlBamPathList = args.controlBamPathList
-    outputPath = args.outputPath
+    starBamFile = args.star
+    ms2BamFile = args.ms2
+    paramInfoFile = args.paramInfoFile
 
 
