7fe14a8a165a5974a3a23ddaa1d0ec1502d6e5d2
friend1ws <friend1ws@gmail.com>
2017-02-20 17:10:50 +0900

    modify version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='fusionfusion',
-      version='0.3.0rc2',
+      version='0.3.0',
       description='Python tools for extracting highly confident fusion transcripts from the results of several RNA-seq alignment tools.',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
